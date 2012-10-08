19a9dc61c83c7638fb8a4796f8df05ed1eeeca50
Dima Stopel <dima@stopel.org>
2012-10-08 23:27:52 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = module.exports = express.createServer();
+app.mongoose = require('mongoose');
+
+var config = require('./config.js')(app, express);
+
+var models = {};
+models.examples = require('./models/example')(app.mongoose);
+
+require('./routes')(app, models, app.mongoose);
+
+app.listen(process.env.PORT || 3000);
---
+++ config.js
@@ -0,0 +1,34 @@
+module.exports = function(app, express, mongoose){
+
+  var config = this;
+
+  //generic config
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.session({ secret: 'topsecret' }));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
+
+  //env specific config
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+    app.mongoose.connect('mongodb://localhost/nodemvr');
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler());
+
+    //app.mongoose.connect('mongodb://flame.mongohq.com:27087/nodemvr');
+    app.mongoose.connect('mongodb://localhost/nodemvr');
+  });
+
+  return config;
+
+};
