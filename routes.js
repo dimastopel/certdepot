7da1b5b314c21725d39bd20fd842fed138e22ea3
Dima Stopel <dima@stopel.org>
2012-10-10 23:07:57 +0200

    starting to gen certs

--- routes.js
+++ routes.js
@@ -1,3 +1,6 @@
+var exec = require('child_process').exec;
+var uuid = require('node-uuid');
+
 module.exports = function(app, models, mongoose){
 
   var title = "Certificate Depot"
@@ -34,6 +37,7 @@ module.exports = function(app, models, mongoose){
         locals: {
           title: title,
           description: description,
+          common_name: req.param('cn'),
           page: 'fillinfo'
         }
     });
@@ -43,6 +47,66 @@ module.exports = function(app, models, mongoose){
    *  Download
    */
   app.post('/download', function(req, res){
+
+    // create certs here
+    var country = req.param("country");
+    var state = req.param("state");
+    var city = req.param("city");
+    var org = req.param("org");
+    var org_unit = req.param("org_unit");
+    var cn = req.param("cn");
+    var email = req.param("email");
+
+    if (cn === "")
+    {
+      console.log("Error: CN is empty. Doing nothing.");
+      return;
+    }
+
+
+    var id = uuid.v4();
+    console.log("uuid: " + id);
+
+    var prefix = "d:\\src\\certdepot\\certs\\" + cn + "$" + id + "$";
+
+    var opts = { encoding: 'utf8',
+                 timeout: 0,
+                 maxBuffer: 200*1024,
+                 killSignal: 'SIGTERM',
+                 cwd: "d:\\Utils\\OpenSSL-Win32\\bin\\",
+                 env: null };
+
+    
+    var command = "openssl.exe genrsa -out " + prefix + "private.txt 1024";  
+    exec(command, opts,
+      function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+      }
+    );
+    
+
+    console.log("generating public cert");
+    command = "openssl.exe req -config \"d:\\Utils\\OpenSSL-Win32\\bin\\openssl.cfg\" -x509 -new -key " + prefix + "private.txt " + " -out " + prefix + "public.txt";  
+    console.log(command);
+    exec(command, opts,
+      function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+      }
+    );
+
+    // FINISHED HERE !!! CAN'T CRATE PUBLIC CERT. NEED TO PROVIDE CN VIA COMMAND LINE. 
+
+    // end
+
+
     //render the index page
     res.render('download.jade', {
         locals: {
