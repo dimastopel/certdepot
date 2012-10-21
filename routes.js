3a7e90c967ef92e6c67a3dc8f41ea1ee0d9542e9
Dima Stopel <dima@stopel.org>
2012-10-21 22:26:49 +0200

    minor fix

--- routes.js
+++ routes.js
@@ -205,7 +205,7 @@ module.exports = function(app, models, mongoose){
                   return;
                 }
 
-                command = "createZip.sh" + names.zip + " " + names.private + " " + names.public;  
+                command = "createZip.sh " + names.zip + " " + names.private + " " + names.public;  
                 exec(command, 
                   function (error, stdout, stderr) {
                     if (error !== null) {
