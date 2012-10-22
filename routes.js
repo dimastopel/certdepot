819eb6fab4ee6fd4dd7e432e62cd8fc3ec2993cc
Dima Stopel <dima@stopel.org>
2012-10-22 21:37:18 +0200

    more cleanup

--- routes.js
+++ routes.js
@@ -27,98 +27,6 @@ module.exports = function(app, models, mongoose){
     });
   //});
   
-  
-  /**
-   *  Fill info
-   */
-  app.post('/fillinfo', function(req, res){
-    //render the index page
-    res.render('fillinfo.jade', {
-        locals: {
-          title: title,
-          description: description,
-          common_name: req.param('cn'),
-          page: 'fillinfo'
-        }
-    });
-  });
-
-  /**
-   *  Download
-   */
-  app.post('/download', function(req, res){
-
-    // create certs here
-    var country = req.param("country");
-    var state = req.param("state");
-    var city = req.param("city");
-    var org = req.param("org");
-    var org_unit = req.param("org_unit");
-    var cn = req.param("cn");
-    var email = req.param("email");
-
-    if (cn === "")
-    {
-      console.log("Error: CN is empty. Doing nothing.");
-      return;
-    }
-
-
-    var id = uuid.v4();
-    console.log("uuid: " + id);
-
-    var prefix = "~/certs/" + cn + "--" + id + "--";
-
-    var opts = { encoding: 'utf8',
-                 timeout: 0,
-                 maxBuffer: 200*1024,
-                 killSignal: 'SIGTERM',
-                 cwd: "d:\\Utils\\OpenSSL-Win32\\bin\\",
-                 env: null };
-
-    
-    var command = "openssl genrsa -out " + prefix + "private.txt 1024";  
-    exec(command, 
-      function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-          console.log('exec error: ' + error);
-          return;
-        }
-      
-        console.log("generating public cert");
-        command = "openssl req -x509 -new -batch -subj \"/commonName=" + cn + "\" -key " + prefix + "private.txt " + " -out " + prefix + "public.txt";  
-        console.log(command);
-        exec(command, 
-          function (error, stdout, stderr) {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (error !== null) {
-              console.log('exec error: ' + error);
-            }
-          }
-        );
-      }
-    );
-    
-
-
-    // FINISHED HERE !!! CAN'T CRATE PUBLIC CERT. NEED TO PROVIDE CN VIA COMMAND LINE. 
-
-    // end
-
-
-    //render the index page
-    res.render('download.jade', {
-        locals: {
-          title: title,
-          description: description,
-          page: 'download'
-        }
-    });
-  });
-
   /**
    *  Help
    */
@@ -280,6 +188,7 @@ module.exports = function(app, models, mongoose){
   /**
    *  Add test doc
    */
+   /*
   app.post('/posts', function(req, res){
      var now = new Date();
      var Post = models.examples;
@@ -293,5 +202,6 @@ module.exports = function(app, models, mongoose){
      });
      res.redirect('/list');
   });
+*/
   
 };
