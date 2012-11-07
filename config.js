798ddc97b561c363950117d8f6a6bdfb4f6b3b7c
Dima Stopel <dima@stopel.org>
2012-11-07 13:49:27 +0200

    add compress middleware

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = function(app, express, mongoose){
   app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
+    app.use(express.compress());
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
