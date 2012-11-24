0a7e50cf63f1fdcc18b2871d9505ebd97b66abdc
Dima Stopel <dima@stopel.org>
2012-11-24 18:34:01 +0200

    Fixing certs path

--- routes.js
+++ routes.js
@@ -6,7 +6,8 @@ module.exports = function(app, models, mongoose){
 
   var title = "Certificate Depot"
   var description = "Create your self-signed SSL certificate instantly and for free."
-  var certPath = "/home/dima/certs/";
+  //var certPath = "/home/dima/certs/";
+  var certPath = "~/certs/";
 
   /**
    *  Index
