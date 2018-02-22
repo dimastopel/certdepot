88b6db3ee9d7a2284fa97958ad71a88ae87fe3b0
Dima Stopel <dima@twistlock.com>
2018-02-22 22:11:22 +0200

    add cert retrieval and count functionality
    Signed-off-by: Dima Stopel <dima@twistlock.com>

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//index.js
 const express = require('express')
 const exec = require('child_process').exec
 const uuid = require('uuid/v4')
@@ -8,114 +7,149 @@ const fs = require('fs')
 const app = express()
 
 const certPath = __dirname + "/certs/"; 
+const counterFileName = certPath + "counter.txt";
 
 app.use(express.static('static'))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/api/create', function (req, res, next) {
-    
-    var cn = req.body.cn
-    var days = req.body.days
-    var country = req.body.country
-    var state = req.body.state
-    var city = req.body.city
-    var org = req.body.org
-    var orgUnit = req.body.orgUnit
-    var email = req.body.email
-    var pfxPass = req.body.pfxPass
+app.post('/api/create', function (req, res) {
 
-    // get other cert info
-    if (!cn || cn === "")
-    {
-      console.log("Error: CN is empty. Doing nothing.")
-      res.status(400).send({error: "Common Name can't be empty"})
+  var cn = req.body.cn
+  var days = req.body.days
+  var country = req.body.country
+  var state = req.body.state
+  var city = req.body.city
+  var org = req.body.org
+  var orgUnit = req.body.orgUnit
+  var email = req.body.email
+  var pfxPass = req.body.pfxPass
+
+  if (!cn || cn === "")
+  {
+    console.log("Error: CN is empty. Doing nothing.")
+    res.status(400).send({error: "Common Name can't be empty"})
+    return;
+  }
+
+  var id = uuid();
+  var names = getCertNames(id);
+
+  var command = "openssl genrsa -out " + names.private + " 1024";  
+  exec(command, 
+    function (error, stdout, stderr) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        res.status(500).send({error: 'Failed to generate private key: ' + error});
+        return;
+      }
+
+      var subj = "/commonName=" + cn;
+      if (country && country != "") {
+        subj += "/C=" + country;
+      }
+      if (state && state != "") {
+        subj += "/ST=" + state;
+      }
+      if (city && city != "") {
+        subj += "/L=" + city;
+      }
+      if (org && org != "") {
+        subj += "/O=" + org;
+      }
+      if (orgUnit && orgUnit != "") {
+        subj += "/OU=" + orgUnit;
+      }
+      if (email && email != "") {
+        subj += "/emailAddress=" + email;
+      }
+
+      console.log("subj: " + subj);
+
+      command = "openssl req -x509 -new -batch -subj " + escapeShell(subj) + " -key " + names.private + " -out " + names.public + " -days " + escapeShell(days);  
+      exec(command, 
+        function (error, stdout, stderr) {
+          if (error !== null) {
+            console.log('exec error: ' + error);
+            res.status(500).send({error: 'Failed to generate public certificate: ' + error});
+            return;
+          }
+
+          if (!pfxPass || pfxPass === "") {
+            pfxPass = "password";
+          }
+
+          command = "openssl pkcs12 -export -inkey " + names.private + " -out " + names.pfx + " -in " + names.public + " -password pass:" + escapeShell(pfxPass);  
+          exec(command, 
+            function (error, stdout, stderr) {
+              if (error !== null) {
+                console.log('exec error: ' + error);
+                res.status(500).send({error: 'Failed to create pfx: ' + error});
+                return;
+              }
+
+              command = "bash createZip.sh " + names.zip_nodir + " " + names.private_nodir + " " + names.public_nodir;  
+              exec(command, 
+                function (error, stdout, stderr) {
+                  if (error !== null) {
+                    console.log('exec error: ' + error);
+                    res.status(500).send({error: 'Failed to create ZIP archive: ' + error});
+                    return;
+                  }
+                  console.log('Successfully generated certs for CN: ' + cn + ' ID:' + id);
+                });
+            });
+        });
+    });
+  incCertCount()
+  res.header('Content-Type', 'application/json');
+  res.send({id:id});
+});
+
+app.get('/api/get/id/:certId/type/:certType', function(req, res) {
+  var id = req.params.certId;
+  var type = req.params.certType;
+  var names = getCertNames(id);
+
+  console.log('Retreiving certs for id: ' + id);
+
+
+  if (type === "zip") {
+
+    if (!fs.existsSync(names.zip)) {
+      console.warn('can not find: ' + names.zip);
+      res.status(404).send({error: 'Can not find cert for id/type: ' + id + "/" + type});
       return;
     }
 
-    var id = uuid();
-    var names = getCertNames(id);
 
-    var command = "openssl genrsa -out " + names.private + " 1024";  
-    exec(command, 
-      function (error, stdout, stderr) {
-        if (error !== null) {
-          console.log('exec error: ' + error);
-          res.status(500).send({error: 'Failed to generate private key: ' + error});
-          return;
-        }
-
-        var subj = "/commonName=" + cn;
-        if (country && country != "") {
-          subj += "/C=" + country;
-        }
-        if (state && state != "") {
-          subj += "/ST=" + state;
-        }
-        if (city && city != "") {
-          subj += "/L=" + city;
-        }
-        if (org && org != "") {
-          subj += "/O=" + org;
-        }
-        if (orgUnit && orgUnit != "") {
-          subj += "/OU=" + orgUnit;
-        }
-        if (email && email != "") {
-          subj += "/emailAddress=" + email;
-        }
-
-        console.log("subj: " + subj);
-
-        command = "openssl req -x509 -new -batch -subj " + escapeShell(subj) + " -key " + names.private + " -out " + names.public + " -days " + escapeShell(days);  
-        exec(command, 
-          function (error, stdout, stderr) {
-            if (error !== null) {
-              console.log('exec error: ' + error);
-              res.status(500).send({error: 'Failed to generate public certificate: ' + error});
-              return;
-            }
-
-            if (!pfxPass || pfxPass === "") {
-              pfxPass = "password";
-            }
-
-            command = "openssl pkcs12 -export -inkey " + names.private + " -out " + names.pfx + " -in " + names.public + " -password pass:" + escapeShell(pfxPass);  
-            exec(command, 
-              function (error, stdout, stderr) {
-                if (error !== null) {
-                  console.log('exec error: ' + error);
-                  res.status(500).send({error: 'Failed to create pfx: ' + error});
-                  return;
-                }
-
-                command = "bash createZip.sh " + names.zip_nodir + " " + names.private_nodir + " " + names.public_nodir;  
-                exec(command, 
-                  function (error, stdout, stderr) {
-                    if (error !== null) {
-                      console.log('exec error: ' + error);
-                      res.status(500).send({error: 'Failed to create ZIP archive: ' + error});
-                      return;
-                    }
-                    console.log('Successfully generated certs for CN: ' + cn + ' ID:' + id);
-                  }
-                  );
-              }
-              );
-          }
-          );
+    res.download(names.zip, names.zip, function(err) {
+      if (err) {
+        console.error('Can not return zip: ' + err);
       }
-      );
+    });
 
-    res.header('Content-Type', 'application/json');
-    res.send({id:id});
-  })
+  } else if (type === "pfx") {
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
-})
+    res.download(names.pfx, names.pfx, function(err) {
+      if (err) {
+        console.error('Can not return pfx: ' + err);
+      }
+    });
 
+  } else {
+    console.warn('Invalid type was sent: ' + type);
+    res.status(400).send({error: 'Can not return cert for this type: ' + type});
+  }
+});
 
+app.get('/api/count', function(req, res) {
 
+  console.log('Retreiving certs count');
+
+  var count = fs.readFileSync(counterFileName, 'utf8');
+
+  res.send({"count":count});
+});
 
 function getCertNames(id) {
   var prefix = certPath + id + ".";
@@ -136,3 +170,23 @@ function getCertNames(id) {
 function escapeShell(cmd) {
   return '"'+cmd.replace(/(["\s'$`\\])/g,'\\$1')+'"';
 };
+
+function incCertCount() {
+
+  if (fs.existsSync(counterFileName)) {
+    var countString = fs.readFileSync(counterFileName);
+    var count = parseInt(countString, 10)
+    if (count != NaN)
+    {
+      fs.writeFileSync(counterFileName, (count+1).toString())
+    }
+  }
+  else
+  {
+    fs.writeFileSync(counterFileName, "0")
+  }
+}
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+})
